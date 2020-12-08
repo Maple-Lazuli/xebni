8460a6434fa371b3ed8467cab62acf6d16da8c69
Maple-Lazuli <the.nostra.tymus@gmail.com>
2020-12-09 05:46:38 +0900

    math calculate paths work. Cannot create tests still

--- src/main/java/com/example/xebni/QuerystringController.java
+++ src/main/java/com/example/xebni/QuerystringController.java
@@ -1,21 +1,29 @@
 package com.example.xebni;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/app")
 public class QuerystringController {
 
-    @GetMapping("/foo")
-    public String getFoo() {
-        return "foo";
+    @GetMapping("/math/calculate")
+    public String getMapParams(@RequestParam HashMap<String, String> querystring){
+        if(querystring.get("operation").contains("add")) {
+            return String.format("%s + %s = %d \n", querystring.get("x"), querystring.get("y"), Integer.valueOf(querystring.get("x")) + Integer.valueOf(querystring.get("y")));
+        }
+        else  if(querystring.get("operation").contains("subtract")) {
+            return String.format("%s - %s = %d\n", querystring.get("x"), querystring.get("y"), Integer.valueOf(querystring.get("x")) - Integer.valueOf(querystring.get("y")));
+        }
+        else if(querystring.get("operation").contains("multiply")) {
+            return String.format("%s * %s = %d\n", querystring.get("x"), querystring.get("y"), Integer.valueOf(querystring.get("x")) * Integer.valueOf(querystring.get("y")));
+        }
+        else if(querystring.get("operation").contains("divide")) {
+            return String.format("%s / %s = %d\n", querystring.get("x"), querystring.get("y"), Integer.valueOf(querystring.get("x")) / Integer.valueOf(querystring.get("y")));
+        }
+        return "Invalid Query String \n";
     }
-
-    @GetMapping("/bar")
-    public String getBar() {
-        return "bar";
-    }
-
 }
--- src/test/java/com/example/xebni/EndpointsControllerTest.java
+++ src/test/java/com/example/xebni/EndpointsControllerTest.java
@@ -1,33 +1,40 @@
-package com.example.xebni;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(EndpointsController.class)
-public class EndpointsControllerTest {
-    @Autowired
-    private MockMvc mvc;
-//    @Test
-//    public void testingPages() throws Exception {
-//        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+//package com.example.xebni;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.RequestBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 //
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(String.valueOf(Math.PI) + "\n"));
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@WebMvcTest(EndpointsController.class)
+//public class EndpointsControllerTest {
+//    @Autowired
+//    private MockMvc mvc;
+////    @Test
+////    public void testingPages() throws Exception {
+////        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+////
+////        this.mvc.perform(request)
+////                .andExpect(status().isOk())
+////                .andExpect(content().string(String.valueOf(Math.PI) + "\n"));
+////    }
+//    @Test
+//    public void testPI() throws Exception {
+//        this.mvc.perform(get("/math/pi"))
+//                .andExpect(status().isOk());
 //    }
-    @Test
-    public void testPI() throws Exception {
-        this.mvc.perform(get("/math/pi"))
-                .andExpect(status().isOk());
-    }
-}
+//
+//    @Test
+//    public void testIndexEndpoint() throws Exception {
+//        this.mvc.perform(get("/vehicles?year=1987&doors=2"))
+//                .andExpect(status().isOk());
+//    }
+//}
